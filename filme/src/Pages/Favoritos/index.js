f514755ab55188e2baefb49f23395a8fcce3560a
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-08-15 15:54:39 -0300

    7º Commit
    versão 1.0 finalizada \o/

--- filme/src/Pages/Favoritos/index.js
+++ filme/src/Pages/Favoritos/index.js
@@ -21,6 +21,11 @@ export default function Favoritos() {
   return (
     <div id="meus-filmes">
       <h1>Meus Filmes</h1>
+
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme salvo :/ </span>
+      )}
+
       <ul>
         {filmes.map((item) => {
           return (
